1da2ed0f7d944ffda137a5e7fef99d4fa5b7cf12
Eugene Datsky <eugene@datsky.ru>
2022-06-20 15:00:00 +1000

    Add story and reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+const getMyVar = () => import.meta.env.VITE_MISSING_VAR === 'true'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +12,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <p>{JSON.stringify(import.meta.env)}</p>
+        <p>{getMyVar()}</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
